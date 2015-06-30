d2616d61f0045d2342e66ba6e48d9847147d4110
Max Schram <maxbschram@gmail.com>
2015-06-30 12:01:45 -0700

    Created basic move functionality for board

--- chess/board.rb
+++ chess/board.rb
@@ -4,6 +4,28 @@ class Board
     @grid = Array.new(8) { Array.new(8) {EmptySquare.new} }
   end
 
+  def in_check?(color)
+    raise "not yet implemented"
+  end
+
+  def move(start, end_pos)
+    self[end_pos] = self[start]
+    self[start] = EmptySquare.new
+  end
+
+  private
+
+  attr_accessor :grid
+  def [](pos)
+    row, col = pos
+    grid[row][col]
+  end
+
+  def []=(pos, val)
+    row, col = pos
+    grid[row][col] = val
+  end
+
 end
 
 class EmptySquare
@@ -11,5 +33,5 @@ class EmptySquare
   def moves
     []
   end
-  
+
 end
